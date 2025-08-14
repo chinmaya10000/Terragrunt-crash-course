6c77868f9ab11192f4b96d0cbc55f79590e64363
chinmaya <chinmaya@gmail.com>
2025-08-14 19:32:58 +0530

    initial commit

---
+++ infrastructure-live-1/dev/vpc/3.subnets.tf
@@ -0,0 +1,49 @@
+resource "aws_subnet" "private-us-east-2a" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.0.0/19"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    "Name" = "dev-private-subnet-us-east-2a"
+    "kubernetes.io/cluster/dev-cluster" = "owned"
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
+resource "aws_subnet" "private-us-east-2b" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.32.0/19"
+  availability_zone = "us-east-2b"
+
+  tags = {
+    "Name" = "dev-private-subnet-us-east-2b"
+    "kubernetes.io/cluster/dev-cluster" = "owned"
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
+resource "aws_subnet" "public-us-east-2a" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.64.0/19"
+  availability_zone = "us-east-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name" = "dev-public-subnet-us-east-2a"
+    "kubernetes.io/cluster/dev-cluster" = "owned"
+    "kubernetes.io/role/elb" = 1
+  }
+}
+
+resource "aws_subnet" "public-us-east-2b" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.96.0/19"
+  availability_zone = "us-east-2b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name" = "dev-public-subnet-us-east-2b"
+    "kubernetes.io/cluster/dev-cluster" = "owned"
+    "kubernetes.io/role/elb" = 1
+  }
+}

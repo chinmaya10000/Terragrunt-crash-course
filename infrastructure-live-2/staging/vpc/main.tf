d95d03be6793a6268e38410fdd679daa8f191c3d
chinmaya <chinmaya@gmail.com>
2025-08-14 21:58:15 +0530

    modularize

--- infrastructure-live-2/staging/vpc/main.tf
+++ infrastructure-live-2/staging/vpc/main.tf
@@ -11,7 +11,7 @@ terraform {
 module "vpc" {
   source = "../../../infrastructure-modules/vpc"
 
-  env = "dev"
+  env = "staging"
   azs = ["us-east-2a", "us-east-2b"]
   private_subnets = ["10.0.0.0/19", "10.0.32.0/19"]
   public_subnets = ["10.0.64.0/19", "10.0.96.0/19"]
